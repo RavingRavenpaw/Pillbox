a8cc0d2eadbc4e662af2c4cbe52d06b8fedebc21
Raven <chromefusion44@gmail.com>
2016-12-14 14:30:53 -0500

    Initial code
    Haven't tested this yet, just threw it together from the browser.

--- main.py
+++ main.py
@@ -1,26 +1,42 @@
-##Initialization
-#
-#Prompt user to set time (5 min intervals) and date
-#Prompt user for alarm 1 time
-#Ask - addional alarms? (y/n)
-#if yes
-#    add alarm time, ask if another alarm neeeded
-#
-#REFULAR BACKGROUND PROCESSES
-#
-#check time every five minutes
-#if not alarm time
-#    do nothing, go back to background process
-#
-#if it is alarm time
-#    Multiprocess, thread, or open multiple files to....
-#    while pillbox switch is pressed down/until it is depressed
-#        1. Display message to take pills on LCD and flash LCD
-#        2. Flash an LED
-#        3. Play music
-#    
-#    When pillbox switch is depressed and user removes box
-#        1. Change LCD back to normal
-#        2. Turn off LED
-#        3. Stop music
-#Go back to background process
+#Initialization - get current time & date
+time = input("Current time: ")
+print("Current date (format MMDDYYYY): ")
+
+#Enter time of 1st alarm and ask if 2nd needed
+alarm1 = input("Time of 1st alarm: ")
+answer = input("Do you need a 2nd alarm? (yes/no)")
+
+#Enter time of 2nd alarm if needed and ask for 3rd
+if answer == yes or answer == y:
+	alarm2 = input("Time of 2nd alarm: ")
+	answer = input("Do you need a 3rd alarm? (yes/no)")
+	
+	#Enter time of 3rd alarm
+	if answer == yes or answer == y:
+		alarm3 = input("Time of 3rd alarm: ")
+
+''' REGULAR BACKGROUND PROCESSES
+VIA THREADING, MULTIPROCESSING, OR RUNNING MULTIPLE PYTHON FILES
+
+
+check time every five minutes or so
+if not alarm time
+    do nothing, go back to background process
+
+if it is alarm time
+    while corresponding pillbox switch is pressed down/until it is depressed
+        1. Display message to take pills on LCD and flash LCD
+        2. Flash an LED
+        3. Play music
+		
+	if wrong pillbox switch depressed
+		1. Change music to a warning tone
+		2. Flash LCD and display message that wrong pillbox was removed
+		3. Wait until switch pressed back in
+
+    When corresponding pillbox switch is depressed and user removes box
+        1. Change LCD back to normal
+        2. Turn off LED
+        3. Stop music
+Go back to background process
+''' 
