42cdc5209deff94f28e75c819a1fb70aff568251
Raven <chromefusion44@gmail.com>
2016-12-14 11:03:31 -0500

    Psuedocode
    It begins.

---
+++ main.py
@@ -0,0 +1,16 @@
+##Initialization
+#
+#Prompt user to set time (5 min intervals) and date
+#Prompt user for alarm 1 time
+#Ask - addional alarms? (y/n)
+#if yes
+#    add alarm time, ask if another alarm neeeded
+#
+#REFULAR BACKGROUND PROCESSES
+#
+#check time every five minutes
+#if not alarm time
+#    do nothing, go back to background process
+#
+#if it is alarm time
+#    cont later......
